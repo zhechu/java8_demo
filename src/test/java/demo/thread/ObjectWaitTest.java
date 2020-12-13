e4ede1fa8351ce1f27b6dc23f4b3a1bb7d849a53
lingyuwang <ling-yu-wang@qq.com>
2020-12-13 22:30:33 +0800

    feature:调用wait方法死锁场景

---
+++ src/test/java/demo/thread/ObjectWaitTest.java
@@ -0,0 +1,98 @@
+package demo.thread;
+
+import org.junit.Test;
+
+public class ObjectWaitTest {
+
+  private static final Object resourceA = new Object();
+  private static final Object resourceB = new Object();
+
+  @Test
+  public void deadlock() throws Exception {
+    // 创建线程
+    Thread threadA = new Thread(() -> {
+      try {
+        // 获取 resourceA 共享资源的监视器锁
+        synchronized (resourceA) {
+          System.out.println("threadA get resourceA lock");
+          // 获取 resourceB 共享资源的监视器锁
+          synchronized (resourceB) {
+            System.out.println("threadA get resourceB lock");
+
+            // 线程 A 阻塞，并释放获取到的 resourceA 锁
+            System.out.println("threadA release resourceA lock");
+            resourceA.wait();
+          }
+        }
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    });
+
+    // 创建线程
+    Thread threadB = new Thread(() -> {
+      try {
+        // 休眠 1 秒
+        Thread.sleep(1000);
+
+        // 获取 resourceA 共享资源的监视器锁
+        synchronized (resourceA) {
+          System.out.println("threadB get resourceA lock");
+
+          // 预期在这发生死锁（threadA 未释放 resourceA 锁）
+
+          // 获取 resourceB 共享资源的监视器锁
+          synchronized (resourceB) {
+            System.out.println("threadB get resourceB lock");
+
+            // 线程 A 阻塞，并释放获取到的 resourceA 锁
+            System.out.println("threadB release resourceA lock");
+            resourceA.wait();
+          }
+        }
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    });
+
+    // 启动线程
+    threadA.start();
+    threadB.start();
+
+    // 等待两个线程结束
+    threadA.join();
+    threadB.join();
+
+    System.out.println("main over");
+  }
+
+  private static final Object obj = new Object();
+
+  @Test
+  public void waitNotifyByInterrupt() throws Exception {
+    // 创建线程
+    Thread threadA = new Thread(() -> {
+      try {
+        System.out.println("begin");
+
+        // 阻塞当前线程
+        synchronized (obj) {
+          obj.wait();
+        }
+
+        System.out.println("end");
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    });
+
+    threadA.start();
+
+    Thread.sleep(1000);
+
+    System.out.println("begin interrupt threadA");
+    threadA.interrupt();
+    System.out.println("end interrupt threadA");
+  }
+
+}

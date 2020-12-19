a2ba77289797e518363c8d9bebdbec06ae867bd3
lingyuwang <ling-yu-wang@qq.com>
2020-12-19 09:59:53 +0800

    feature:unsafe类的使用

---
+++ src/test/java/demo/misc/UnsafeTest.java
@@ -0,0 +1,41 @@
+package demo.misc;
+
+import org.junit.Test;
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+public class UnsafeTest {
+
+  @Test
+  public void compareAndSwapLong() {
+    System.out.println("start ....");
+
+    UnsafeTest unsafeTest = new UnsafeTest();
+
+    boolean success = unsafe.compareAndSwapLong(unsafeTest, stateOffset, 0, 1);
+
+    System.out.println(success);
+
+    System.out.println("end ....");
+  }
+
+  static Unsafe unsafe;
+
+  static final long stateOffset;
+
+  private volatile long state = 0;
+
+  static {
+    try {
+      Field field = Unsafe.class.getDeclaredField("theUnsafe");
+      field.setAccessible(true);
+      unsafe = (Unsafe) field.get(null);
+      stateOffset = unsafe.objectFieldOffset(UnsafeTest.class.getDeclaredField("state"));
+    } catch (Exception ex) {
+      System.out.println(ex.getLocalizedMessage());
+      throw new Error(ex);
+    }
+  }
+
+}

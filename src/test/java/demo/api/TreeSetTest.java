314359b8be457f4ce068fa01a3d8e54e4203ded0
lingyuwang <ling-yu-wang@qq.com>
2021-01-19 21:31:54 +0800

    feature:排序Set集合

--- src/test/java/demo/api/TreeSetTest.java
+++ src/test/java/demo/api/TreeSetTest.java
@@ -1,9 +1,11 @@
 package demo.api;
 
+import lombok.*;
 import org.junit.Test;
 
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.stream.Collectors;
 
 public class TreeSetTest {
 
@@ -18,6 +20,155 @@ public class TreeSetTest {
     resultSet.forEach(System.out::println);
 
     System.out.println(sortedSet.size());
+  }
+
+  /**
+   * 实现按分值排序
+   */
+  @Test
+  public void scoreSort() {
+    TreeSet<CacheData> treeSet = new TreeSet<>((o1, o2) -> {
+      if (o1.getScore() == o2.getScore()) {
+        return 0;
+      }
+      return o1.getScore() > o2.getScore() ? 1 : -1;
+    });
+
+    Random random = new Random();
+    for (int i = 0; i < 100; i++) {
+      treeSet.add(new CacheData(String.valueOf(i), random.nextDouble()));
+    }
+
+    System.out.println("=================================");
+
+    treeSet.forEach(System.out::println);
+
+    // 覆盖
+    treeSet.add(new CacheData("99", random.nextDouble()));
+
+    System.out.println("=================================");
+
+    treeSet.stream().filter(o -> o.getId().equals("99")).forEach(System.out::println);
+
+  }
+
+  @Test
+  public void set() {
+    Set<CacheData> hashSet = new HashSet<>();
+
+    Random random = new Random();
+    for (int i = 0; i < 100; i++) {
+      hashSet.add(new CacheData(String.valueOf(i), random.nextDouble()));
+    }
+
+    System.out.println("=================================");
+
+    hashSet.forEach(System.out::println);
+
+    // 覆盖
+    hashSet.add(new CacheData("99", random.nextDouble()));
+
+    System.out.println("=================================");
+
+    hashSet.stream().filter(o -> o.getId().equals("99")).forEach(System.out::println);
+  }
+
+  /**
+   * 实现根据分值排序集合并使用分页查询
+   */
+  @Test
+  public void sortedSet() {
+    Set<CacheData> sortedSet = new ConcurrentSkipListSet<>((o1, o2) -> {
+      if (o1.getScore() == o2.getScore()) {
+        return 0;
+      }
+      return o1.getScore() > o2.getScore() ? 1 : -1;
+    });
+
+    Random random = new Random();
+    for (int i = 0; i < 100; i++) {
+      sortedSet.add(new CacheData(String.valueOf(i), random.nextDouble()));
+    }
+
+    int page = 1;
+    int pageSize = 10;
+    List<CacheData> resultList = sortedSet.stream()
+        .skip((page - 1) * pageSize)
+        .limit(pageSize)
+        .collect(Collectors.toList());
+    resultList.forEach(System.out::println);
+
+    System.out.println("=================================");
+
+    sortedSet.forEach(System.out::println);
+
+    // 覆盖
+    sortedSet.add(new CacheData("99", random.nextDouble()));
+
+    System.out.println("=================================");
+
+    sortedSet.stream().filter(o -> o.getId().equals("99")).forEach(System.out::println);
+
+    CacheData cacheDataA = new CacheData("99", random.nextDouble());
+    CacheData cacheDataB = new CacheData("99", random.nextDouble());
+    System.out.println(cacheDataA.equals(cacheDataB));
+    System.out.println(cacheDataA.hashCode() + "=" + cacheDataB.hashCode());
+  }
+
+  @Test
+  public void concurrentSkipListSet() {
+    Set<String> set = new ConcurrentSkipListSet<>(String::compareTo);
+    for (int i = 0; i < 100; i++) {
+      set.add(String.valueOf(i));
+    }
+
+    set.forEach(System.out::println);
+
+    // 覆盖
+    set.add("99");
+
+    System.out.println("=================================");
+
+    set.forEach(System.out::println);
+
+    System.out.println("=================================");
+
+    set.stream().filter(o -> o.equals("99")).forEach(System.out::println);
+  }
+
+}
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+class CacheData {
+
+  private String id;
+
+  private double score;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null)
+      return false;
+    if (getClass() != o.getClass())
+      return false;
+    CacheData other = (CacheData) o;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return id == null ? 0 : id.hashCode();
   }
 
 }

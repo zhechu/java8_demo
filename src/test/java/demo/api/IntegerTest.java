7d9a8e2739c95c0808cb42374321bee4c8c4f6f7
lingyuwang <ling-yu-wang@qq.com>
2021-01-24 20:08:25 +0800

    feature:规范时间轮每轮的大小

--- src/test/java/demo/api/IntegerTest.java
+++ src/test/java/demo/api/IntegerTest.java
@@ -12,4 +12,69 @@ public class IntegerTest {
     System.out.println(Integer.bitCount(10));
   }
 
+  /**
+   * 求倍数
+   */
+  @Test
+  public void getMultiple() {
+    int mod = 16;
+    int target = 16;
+    int bitCount = Integer.bitCount(mod - 1);
+    System.out.println("bitCount:" + bitCount);
+    int result = target >> bitCount;
+    System.out.println("result:" + result);
+  }
+
+  /**
+   * 规范时间轮每轮的大小
+   */
+  @Test
+  public void normalizeTicksPerWheel() {
+    for (int i = 0; i < 100; i++) {
+      System.out.println("ticksPerWheel:" + i + ", normalizedTicksPerWheel:" + netty(i));
+      System.out.println("ticksPerWheel:" + i + ", normalizedTicksPerWheel:" + hashMap(i));
+      System.out.println("ticksPerWheel:" + i + ", normalizedTicksPerWheel:" + integer(i));
+      System.out.println("================================");
+    }
+  }
+
+  private int integer(int ticksPerWheel) {
+    if (ticksPerWheel <= 1) {
+      return ticksPerWheel;
+    }
+
+    int n = ticksPerWheel - 1;
+
+    int normalizedTicksPerWheel = Integer.highestOneBit(n);
+    normalizedTicksPerWheel <<= 1;
+    return normalizedTicksPerWheel;
+  }
+
+  private int hashMap(int ticksPerWheel) {
+    if (ticksPerWheel <= 1) {
+      return ticksPerWheel;
+    }
+
+    int n = ticksPerWheel - 1;
+    n |= n >>> 1;
+    n |= n >>> 2;
+    n |= n >>> 4;
+    n |= n >>> 8;
+    n |= n >>> 16;
+    int normalizedTicksPerWheel = n + 1;
+    return normalizedTicksPerWheel;
+  }
+
+  private int netty(int ticksPerWheel) {
+    if (ticksPerWheel <= 1) {
+      return ticksPerWheel;
+    }
+
+    int normalizedTicksPerWheel = 1;
+    while (normalizedTicksPerWheel < ticksPerWheel) {
+      normalizedTicksPerWheel <<= 1;
+    }
+    return normalizedTicksPerWheel;
+  }
+
 }
